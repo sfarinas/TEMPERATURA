7040046305a93bd7a574f2171ac6b135e8af8de9
Salomao Dantas Farina <sfarinas@gmail.com>
2023-09-10 18:44:38 -0300

    Update converte.java

--- Main.java
+++ Main.java
@@ -1,18 +1,18 @@
 import java.util.Scanner;
 
-public class Main {
+public class converte {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        // CONTANTES AJUTE E FATOR
         final double AJUSTE = 32.0;
-        final double FATOR = 5.0 / 9.0; // 5/9
-        //formula : (F - 32 x 5/9 = C )
+        final double FATOR = 5.0 / 9.0;
 
-        System.out.print("Digite a temperatura em fahenheit : ");
+        System.out.print("Digite o falor em fahenheit : ");
         double fahenheit = entrada.nextDouble();
 
-        double celsius = (fahenheit - AJUSTE) * FATOR;
-        System.out.println("Temperatura em Celsis: " + celsius);
+        double celcis = (fahenheit - AJUSTE) * FATOR;
+        String resultado = String.format("%.2f", celcis);
+
+        System.out.println("Temperatura em celcis 'e "+ resultado);
 
         entrada.close();
 
